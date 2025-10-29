e19eb4295c2af236f3497dc122cf0d3f7de6178a
PiaCarolin4 <pi.peralta@duocuc.cl>
2025-10-28 21:29:23 -0300

    Actualiza vista de productos

--- src/pages/Productos.jsx
+++ src/pages/Productos.jsx
@@ -1,19 +1,47 @@
-export default function Productos(){ return(
-    <nav>
-        <main class="container py-5">
-    <h1 class="section-title mb-4">Catálogo</h1>
+import { useState } from "react";
+import ProductCard from "../components/ProductCard.jsx";
 
-   
-    <div class="input-group mb-4">
-      <span class="input-group-text"><i class="bi bi-search"></i></span>
-      <input id="qProducts" class="form-control" placeholder="Buscar por nombre, categoría o código"></input>
-    </div>
+import imgDragonBall from "../assets/img/dragonballsparkin.png";
+import imgZelda from "../assets/img/zeldataers.jpg";
+import imgMario from "../assets/img/mario64.jpg";
 
-    
-    <div id="catalogo" class="row g-3"></div>
+export default function Productos(){ 
+  
+  const [query, setQuery] = useState("");
+  const [products] = useState([
+    { id: 1, title: "Dragon Ball: Sparking Zero", price: 59990, imgUrl: imgDragonBall, tag: "PS5" },
+    { id: 2, title: "Zelda: Tears of the Kingdom", price: 49990, imgUrl: imgZelda, tag: "Switch" },
+    { id: 3, title: "Mario 64", price: 39990, imgUrl: imgMario, tag: "N64" }
+  ]);
+
+   const filtrados = products.filter(p =>
+    p.title.toLowerCase().includes(query.toLowerCase()) ||
+    p.tag.toLowerCase().includes(query.toLowerCase())
+  );
+  
+  return (
+    <main>
+      <main className="container py-5">
+        <h1 className="section-title mb-4">Catálogo</h1>
+
+        <div className="input-group mb-4">
+            <span className="input-group-text">
+              <i className="bi bi-search" />
+            </span>
+            <input
+              className="form-control"
+              placeholder="Buscar por nombre o consola…"
+              value={query}
+              onChange={e => setQuery(e.target.value)}
+            />
+        </div>
+
+        <div className="row g-3">
+            {filtrados.map(p => (
+              <ProductCard key={p.id} {...p} />
+            ))}
+        </div>
+      </main>
   </main>
-
-  
-  
-    </nav>
-) }
+  );
+}
